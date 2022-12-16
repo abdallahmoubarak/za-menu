c176664b0610de1407ff003bc289cc4c71af225e
Abdallah <abdallah.moubarak02@gmail.com>
2022-12-17 00:17:49 +0200

    ZM-1 setting up the base layout

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
